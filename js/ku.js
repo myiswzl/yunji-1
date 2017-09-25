72d648ed643669070bd4bea1bbb15f6efa4b4232
mywzl <610850645@qq.com>
2017-09-25 23:13:55 +0800

    云吉

---
+++ js/ku.js
@@ -0,0 +1,28 @@
+/*
+* @Author: Shinelon
+* @Date:   2017-04-26 11:22:12
+* @Last Modified by:   Shinelon
+* @Last Modified time: 2017-04-27 18:25:43
+*/
+
+'use strict';
+function product(title,des,price){
+                  this.title=title;
+                  this.des=des;
+                  this.price=price;
+                  this.buy=function(bag){
+                  	alert('确认购买吗？'+'bag');
+                  }
+
+
+}
+
+function(title,des,price,model){
+	this.title=tetle;
+	this.des=des;
+	this.price=price;
+	this.model=model;
+	this.buy=function(){
+		alert('确认购买吗？'+'bag')
+	};
+}        	
